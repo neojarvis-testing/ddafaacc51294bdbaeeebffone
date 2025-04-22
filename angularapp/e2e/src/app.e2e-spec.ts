c7a6ac30693975fd3b7b12c7d31ebc196476f814
neojarvis <techadmin@iamneo.ai>
2025-04-22 11:04:44 +0000

    initialCommit-Boilerplate

---
+++ angularapp/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,27 @@
+import { sourceMapsEnabled } from 'process';
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('angularapp app is running!');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
+sourceMapsEnabled
+if(a=b);
+close
